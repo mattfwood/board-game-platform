769bd159c1f76172abe93a02c3573001178e4258
Matt Wood <matt@cratebind.com>
2021-04-15 10:53:06 -0500

    Add server, lobby, tictactoe demo

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,66 @@
 import Link from 'next/link'
+import { Client, Lobby } from 'boardgame.io/react';
+import { SocketIO } from 'boardgame.io/multiplayer'
+import { TicTacToe } from '../lib/Game';
+import { TicTacToeBoard } from '../components/Board';
+import { useEffect, useState } from 'react';
+import { LobbyClient } from 'boardgame.io/client';
+import { useAsync } from '../hooks/useAsync';
+
+const lobbyClient = new LobbyClient({ server: 'http://localhost:8000' });
+
+const asyncFunction = async () => {
+  const result = await lobbyClient.listGames();
+  return result;
+}
+
+const GameClient = Client({ game: TicTacToe, board: TicTacToeBoard, multiplayer: SocketIO({ server: 'localhost:8000' }) })
+
+const useLobby = () => {
+  const result = useAsync(asyncFunction)
+
+  return result;
+}
+
+class App extends React.Component {
+  state = { playerID: null };
+
+  render() {
+    if (this.state.playerID === null) {
+      return (
+        <div>
+          <p>Play as</p>
+          <button onClick={() => this.setState({ playerID: "0" })}>
+            Player 0
+          </button>
+          <button onClick={() => this.setState({ playerID: "1" })}>
+            Player 1
+          </button>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <GameClient playerID={this.state.playerID} />
+      </div>
+    );
+  }
+}
+
 
 export default function Home() {
+  const result = useLobby();
+
+  console.log(result);
   return (
-    <ul>
-      <li>
-        <Link href="/b" as="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/a" as="/b">
-          <a>b</a>
-        </Link>
-      </li>
-    </ul>
+    <div>
+      <Lobby
+        gameServer={`http://localhost:8000`}
+        lobbyServer={`http://localhost:8000`}
+        gameComponents={[
+                { game: TicTacToe, board: TicTacToeBoard }
+        ]}
+      />
+    </div>
   )
 }
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const { createServer } = require('http')
-const next = require('next')
+
+const { createServer } = require('http');
+const next = require('next');
+const { Server } = require('boardgame.io/server');
+
+const { TicTacToe } = require('./lib/Game');
+const multiplayerServer = Server({ games: [TicTacToe] });
+
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+
+multiplayerServer.run(8000);
 
 app.prepare().then(() => {
   createServer((req, res) => {
