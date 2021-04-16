5fd281e9e05550199c502a16e5d3a11320f8a7c6
Matt Wood <matt@cratebind.com>
2021-04-16 14:47:42 -0500

    Add separate server process

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: npm run start
+worker: node -r esm server.js
---
+++ old_server.js
@@ -0,0 +1,63 @@
+const Router = require('@koa/router');
+// const { createServer } = require('http');
+const next = require('next');
+const { Server } = require('boardgame.io/server');
+
+const { TicTacToe } = require('./lib/Game');
+
+const appPort = parseInt(process.env.PORT, 10) || 3000;
+const apiPort = parseInt(process.env.API_PORT, 10) || 8000;
+// const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+// gameServer.run(appPort);
+
+app.prepare().then(() => {
+  const gameServer = Server({ games: [TicTacToe] });
+  // const router = new Router();
+  const { router, app } = gameServer;
+
+  router.all('/multiplayer', async ctx => {
+    console.log(ctx);
+    ctx.respond = false;
+  });
+
+  router.all('(.*)', async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+
+  app.use(async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+  });
+
+  app.use(router.routes());
+
+  gameServer.run({
+    port: appPort,
+    lobbyConfig: { apiPort },
+  });
+});
+
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     const parsedUrl = new URL(req.url, 'http://w.w');
+//     const { pathname, query } = parsedUrl;
+
+//     handle(req, res, parsedUrl);
+
+//     // if (pathname === '/a') {
+//     //   app.render(req, res, '/a', query)
+//     // } else if (pathname === '/b') {
+//     //   app.render(req, res, '/b', query)
+//     // } else {
+//     //   handle(req, res, parsedUrl)
+//     // }
+//   }).listen(appPort, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${appPort}`);
+//   });
+// });
