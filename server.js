1876f82bfd5069d21366f86624e3346af3283ed1
Matt Wood <matt@cratebind.com>
2021-04-16 13:39:57 -0500

    Change server.js to serve multiplayer + next server

--- server.js
+++ server.js
@@ -1,35 +1,57 @@
-
-const { createServer } = require('http');
+const Router = require('@koa/router');
+// const { createServer } = require('http');
 const next = require('next');
 const { Server } = require('boardgame.io/server');
 
 const { TicTacToe } = require('./lib/Game');
-const multiplayerServer = Server({ games: [TicTacToe] });
 
+const appPort = parseInt(process.env.PORT, 10) || 3000;
+const apiPort = parseInt(process.env.API_PORT, 10) || 8000;
+// const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
-
-multiplayerServer.run(port);
+// gameServer.run(appPort);
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = new URL(req.url, 'http://w.w')
-    const { pathname, query } = parsedUrl
+  const gameServer = Server({ games: [TicTacToe] });
+  const router = new Router();
 
-    handle(req, res, parsedUrl)
+  router.all('(.*)', async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
 
-    // if (pathname === '/a') {
-    //   app.render(req, res, '/a', query)
-    // } else if (pathname === '/b') {
-    //   app.render(req, res, '/b', query)
-    // } else {
-    //   handle(req, res, parsedUrl)
-    // }
-  }).listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+  gameServer.app.use(async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+  });
+
+  gameServer.app.use(router.routes());
+
+  gameServer.run({
+    port: appPort,
+    lobbyConfig: { apiPort },
+  });
+});
+
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     const parsedUrl = new URL(req.url, 'http://w.w');
+//     const { pathname, query } = parsedUrl;
+
+//     handle(req, res, parsedUrl);
+
+//     // if (pathname === '/a') {
+//     //   app.render(req, res, '/a', query)
+//     // } else if (pathname === '/b') {
+//     //   app.render(req, res, '/b', query)
+//     // } else {
+//     //   handle(req, res, parsedUrl)
+//     // }
+//   }).listen(appPort, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${appPort}`);
+//   });
+// });
