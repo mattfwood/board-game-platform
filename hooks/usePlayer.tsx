778dca17c0505f195b5772e813e82beeb8efbe07
Matt Wood <matt@cratebind.com>
2021-04-19 11:55:26 -0500

    Change usePlayer to not update cookie when player is null

--- hooks/usePlayer.tsx
+++ hooks/usePlayer.tsx
@@ -17,7 +17,9 @@ export const usePlayer = () => {
   const [player, setPlayer] = useRecoilState<Player>(playerState);
 
   useEffect(() => {
-    Cookies.save('playerState', player, { path: '/' });
+    if (player !== null) {
+      Cookies.save('playerState', player, { path: '/' });
+    }
   }, [player]);
 
   return [player, setPlayer] as const;
